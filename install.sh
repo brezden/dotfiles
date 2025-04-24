04ed756f89d2a11116216ca21be1965131bc6642
callumbrezden@gmail.com <callumbrezden@gmail.com>
2025-04-23 21:43:06 -0700

    Godot and Ghostty

--- install.sh
+++ install.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+mkdir -p "$HOME/.config"
+
 echo "Linking .ideavimrc"
-ln -sf "$PWD/ideavimrc" "$HOME/.ideavimrc"
+[ ! -L ~/.ideavimrc ] && rm -f ~/.ideavimrc && ln -s ~/dotfiles/ideavimrc ~/.ideavimrc
+
+echo "Linking Godot config"
+[ ! -L ~/.config/godot ] && rm -rf ~/.config/godot && ln -s ~/dotfiles/godot ~/.config/godot
+
+echo "Linking Ghostty config"
+[ ! -L ~/.config/ghostty ] && rm -rf ~/.config/ghostty && ln -s ~/dotfiles/ghostty ~/.config/ghostty
 
